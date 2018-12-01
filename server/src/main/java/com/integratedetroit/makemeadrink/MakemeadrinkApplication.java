487cdb315d7761c921debb4d2d8000ba6b8c4b86
Cole Wyman <cole@integral.io>
2018-11-30 20:09:31 -0500

    initial commit; add vue boilerplate from cli and spring initializr project

---
+++ server/src/main/java/com/integratedetroit/makemeadrink/MakemeadrinkApplication.java
@@ -0,0 +1,12 @@
+package com.integratedetroit.makemeadrink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakemeadrinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakemeadrinkApplication.class, args);
+	}
+}
---
+++ server/src/test/java/com/integratedetroit/makemeadrink/MakemeadrinkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.integratedetroit.makemeadrink;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MakemeadrinkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
