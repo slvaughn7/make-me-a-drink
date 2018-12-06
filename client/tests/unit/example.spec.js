424cf8e435f7ff00a2fe2b5ba30572bbcd2d594a
Kim D Sanders <kimdionnesanders@gmail.com>
2018-12-05 20:46:16 -0500

    Test for card 10. Index page added

--- client/tests/unit/example.spec.js
+++ client/tests/unit/example.spec.js
@@ -1,12 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
+import homepage from "@/components/Index.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe("Index.vue", () => {
+  it("drink logo click directs back to homepage", () => {
+    const button = "v-on:click";
+    expect(button).toBe("v-on:click");
   });
 });
