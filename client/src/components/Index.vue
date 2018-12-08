391c83d850b472e9f333f5caa2c2279de99b5806
Kim D Sanders <kimdionnesanders@gmail.com>
2018-12-07 21:00:20 -0500

    Changed test name.  Cleaned up index.vue.

--- client/src/components/Index.vue
+++ client/src/components/Index.vue
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="icon" href="<%= BASE_URL %>favicon.ico">
-    <title>make-me-a-drink</title>
-  </head>
+<template>
 
-  <button v-on:click= http://localhost:8080/ >Drink</button>   
+  <button name:logo v-on:click="'http://localhost:8080/'">Drink</button>   
 
-    <div id="app"></div>
-    <!-- built files will be auto injected -->
-  </body>
-  
-</html>
+ </template>
 
+ <script>
+export default {
+  name: "Index",
+  props: {
+    msg: String
+  }
+};
+</script>
 
 
  
---
+++ client/tests/unit/index.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from "@vue/test-utils";
+import Index from "@/components/Index.vue";
+
+describe("Index.vue", () => {
+  it("drink logo click directs back to homepage", () => {
+    const button = "v-on:click";
+    expect(button).toBe("v-on:click");
+  });
+});
