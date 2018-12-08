b9cdf08701f39163c0a057aa3f0166e235f504fb
Kim D Sanders <kimdionnesanders@gmail.com>
2018-12-08 17:09:28 -0500

    renamed test for homepage button

--- client/tests/unit/index.spec.js
+++ client/tests/unit/index.spec.js
@@ -1,8 +1,16 @@
-import { shallowMount } from "@vue/test-utils";
-import Index from "@/components/Index.vue";
+//import { shallowMount } from "@vue/test-utils";
+//import Index from "@/components/Index.vue";
 
 describe("Index.vue", () => {
   it("drink logo click directs back to homepage", () => {
+    const button = "v-on:click";
+    expect(button).toBe("v-on:click");
+  });
+});
+
+
+describe("Recipe.vue", () => {
+  it("make me a drink click directs to recipe page", () => {
     const button = "v-on:click";
     expect(button).toBe("v-on:click");
   });
